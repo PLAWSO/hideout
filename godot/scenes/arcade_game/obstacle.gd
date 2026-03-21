6d22dd2d0fe2a2931c1001567101cd145ec50a3f
PLAWSO <lawson.payton76@gmail.com>
2026-03-20 19:37:11 -0500

    Added sounds! Storing and displaying personal bests. Having ships fly past the screen when the player dies.

--- godot/scenes/arcade_game/obstacle.gd
+++ godot/scenes/arcade_game/obstacle.gd
@@ -1,8 +1,9 @@
 class_name Obstacle extends Node2D
 
 var speed: float = 400.0
+var speed_tween: Tween = null
 
 @onready var hit_box: Area2D = $HitBox
 
 func move(delta: float) -> void:
-	position.x -= speed * delta
+	position.x -= speed * delta  
